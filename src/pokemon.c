f1d2021e4ddff52f1c2e6a2e8497886f75aef72a
hondew <pokehondew@gmail.com>
2021-05-09 14:37:34 -0400

    Fix revive

--- src/pokemon.c
+++ src/pokemon.c
@@ -4931,19 +4931,24 @@ bool8 PokemonUseItemEffects(struct Pokemon *mon, u16 item, u8 partyIndex, u8 mov
                             }
                             if (gMain.inBattle)
                             {
-                                if (battlerId != MAX_BATTLERS_COUNT)
-                                {
-                                    gAbsentBattlerFlags &= ~gBitTable[battlerId];
-                                    CopyPlayerPartyMonToBattleData(battlerId, GetPartyIdFromBattlePartyId(gBattlerPartyIndexes[battlerId]));
-                                    if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER && gBattleResults.numRevivesUsed < 255)
-                                        gBattleResults.numRevivesUsed++;
-                                }
-                                else
-                                {
-                                    gAbsentBattlerFlags &= ~gBitTable[gActiveBattler ^ 2];
-                                    if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER && gBattleResults.numRevivesUsed < 255)
-                                        gBattleResults.numRevivesUsed++;
-                                }
+                                if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER && gBattleResults.numRevivesUsed < 255)
+                                    gBattleResults.numRevivesUsed++;
+
+                                // Reviving a mon should not open up a spot to switch in during double battles
+                                // if (battlerId != MAX_BATTLERS_COUNT)
+                                // {
+                                //     gAbsentBattlerFlags &= ~gBitTable[battlerId];
+                                //     CopyPlayerPartyMonToBattleData(battlerId, GetPartyIdFromBattlePartyId(gBattlerPartyIndexes[battlerId]));
+                                //     if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER && gBattleResults.numRevivesUsed < 255)
+                                //         gBattleResults.numRevivesUsed++;
+                                // }
+                                // else
+                                // {
+                                //     // If partner was absent (dead) mark it as present
+                                //     gAbsentBattlerFlags &= ~gBitTable[gActiveBattler ^ 2];
+                                //     if (GetBattlerSide(gActiveBattler) == B_SIDE_PLAYER && gBattleResults.numRevivesUsed < 255)
+                                //         gBattleResults.numRevivesUsed++;
+                                // }
                             }
                         }
                         else
